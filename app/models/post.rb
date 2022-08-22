4c4b674b5f6c7d59eb89d19fac6ae402ad4c385c
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-08-22 02:20:00 -0700

    Add associations between Comment, User and Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
   URL_REGEXP = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
   validates :url, format: { with: URL_REGEXP, message: 'You provided invalid URL' }
   validates :url, :user_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
   validates :username, :password, :email, presence: true
   validates :username, :email, uniqueness: true
   validates :username, :password, length: { in: 6..20 }
