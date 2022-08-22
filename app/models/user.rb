bfd237b52a35410986e571c37d4b5038744d143b
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-08-22 01:16:11 -0700

    Add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, :password, :email, presence: true
+  validates :username, :email, uniqueness: true
+  validates :username, :password, length: { in: 6..20 }
 end
