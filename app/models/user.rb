c9948d12b9cedb505e62967376b3671091a77551
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-08-22 02:03:55 -0700

    Add associations between User and Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :username, :password, :email, presence: true
   validates :username, :email, uniqueness: true
   validates :username, :password, length: { in: 6..20 }
